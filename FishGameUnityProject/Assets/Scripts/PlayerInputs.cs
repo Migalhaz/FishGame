94e884c401d237f3435ac69839ee79be9d6556bc
NucleoJogos <nucleojogosifrncm@outlook.com.br>
2023-09-12 12:59:42 -0300

    script de input

---
+++ FishGameUnityProject/Assets/Scripts/PlayerInputs.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Character
+{
+    public class PlayerInputs : MonoBehaviour
+    {
+        Vector2 m_moveDir;
+        public Vector2 m_MoveDir => m_moveDir;
+        void Start()
+        {
+
+        }
+        void Update()
+        {
+
+        }
+    }
+}
